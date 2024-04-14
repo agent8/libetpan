e0d4cc23cc6c9d422be33cadb1f9012dadab807e
Liang Wang <wangliang1124@163.com>
2024-04-14 20:59:35 +0800

    improve script, include iphonesimulator arm64, build sasl2.xcframework

--- build-mac/dependencies/prepare-cyrus-sasl.sh
+++ build-mac/dependencies/prepare-cyrus-sasl.sh
@@ -24,6 +24,8 @@ logdir="$tempbuilddir/log"
 resultdir="$builddir/builds"
 tmpdir="$tempbuilddir/tmp"
 
+BUILD="$(uname -m)"
+
 mkdir -p "$resultdir"
 mkdir -p "$logdir"
 mkdir -p "$tmpdir"
@@ -69,14 +71,14 @@ fi
 logfile="$srcdir/$ARCHIVE/build.log"
 
 echo "*** patching sources ***" > "$logfile" 2>&1
-
 cd "$srcdir/$ARCHIVE"
-patch -p1 < $current_dir/$patchfile
 # patch source files
+patch -p1 < $current_dir/$patchfile
+
 cd "$srcdir/$ARCHIVE/include"
-sed -E 's/\.\/makemd5 /.\/makemd5i386 /' < Makefile.am > Makefile.am.new
+sed -E 's/\.\/\$< /.\/\$<'$BUILD' /' < Makefile.am > Makefile.am.new
 mv Makefile.am.new Makefile.am
-sed -E 's/\.\/makemd5 /.\/makemd5i386 /' < Makefile.in > Makefile.in.new
+sed -E 's/\.\/\$< /.\/\$<'$BUILD' /' < Makefile.in > Makefile.in.new
 mv Makefile.in.new Makefile.in
 cd "$srcdir/$ARCHIVE/lib"
 sed -E 's/\$\(AR\) cru \.libs\/\$@ \$\(SASL_STATIC_OBJS\)/&; \$\(RANLIB\) .libs\/\$@/' < Makefile.in > Makefile.in.new
@@ -99,9 +101,10 @@ if [[ "$?" != "0" ]]; then
   echo "BUILD FAILED"
   exit 1
 fi
+
 cd ..
-echo generated makemd5i386 properly
-mv "$srcdir/$ARCHIVE/include/makemd5" "$srcdir/$ARCHIVE/include/makemd5i386"
+echo generated makemd5$BUILD properly
+mv "$srcdir/$ARCHIVE/include/makemd5" "$srcdir/$ARCHIVE/include/makemd5$BUILD"
 make clean >>"$logfile" 2>&1
 make distclean >>"$logfile" 2>&1
 find . -name config.cache -print0 | xargs -0 rm
@@ -122,21 +125,25 @@ if test "x$NOBITCODE" != x ; then
    BITCODE_FLAGS=""
 fi
 
+xcode_developer="$(xcode-select -p)"
+
 for TARGET in $TARGETS; do
 
-    DEVELOPER="$(xcode-select --print-path)"
     SDK_ID="$(echo "$TARGET$SDK_IOS_VERSION" | tr A-Z a-z)"
     SYSROOT="$(xcodebuild -version -sdk "$SDK_ID" 2>/dev/null | egrep '^Path: ' | cut -d ' ' -f 2)"
+
     case $TARGET in
         (iPhoneOS) 
             ARCH=arm
-            MARCHS="armv7 armv7s arm64"
+            MARCHS="arm64"
             EXTRA_FLAGS="$BITCODE_FLAGS -miphoneos-version-min=$SDK_IOS_MIN_VERSION"
+            TARGET_SUFFIX=""
             ;;
         (iPhoneSimulator)
             ARCH=i386
-            MARCHS="i386 x86_64"
-            EXTRA_FLAGS="-miphoneos-version-min=$SDK_IOS_MIN_VERSION"
+            MARCHS="x86_64 arm64"
+            EXTRA_FLAGS="$BITCODE_FLAGS -miphoneos-version-min=$SDK_IOS_MIN_VERSION"
+            TARGET_SUFFIX="-simulator"
             ;;
     esac
     
@@ -145,12 +152,19 @@ for TARGET in $TARGETS; do
                 echo "*** building for $TARGET - $MARCH ***" >> "$logfile" 2>&1
             
         PREFIX=${BUILD_DIR}/${LIB_NAME}/${TARGET}${SDK_IOS_VERSION}${MARCH}
+        CURRENT_TARGET="$MARCH-apple-ios${TARGET_SUFFIX}"
         rm -rf $PREFIX
-        export CPPFLAGS="-arch ${MARCH} -isysroot ${SYSROOT}"
-        export CFLAGS="${CPPFLAGS} -Os ${EXTRA_FLAGS}"
+
+        export CPPFLAGS="-isysroot ${SYSROOT} -target ${CURRENT_TARGET} -Os"
+        export CFLAGS="${CPPFLAGS} ${EXTRA_FLAGS}"
+        # export CC="$xcode_developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang"
+        # export CXX="$xcode_developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
+        # export LD="$xcode_developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ld"
+        # export AR="$xcode_developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ar"
+
         OPENSSL="--with-openssl=$BUILD_DIR/openssl-1.0.0d/universal"
         PLUGINS="--enable-otp=no --enable-digest=no --with-des=no --enable-login"
-        ./configure --host=${ARCH} --prefix=$PREFIX --enable-shared=no --enable-static=yes --with-pam=$BUILD_DIR/openpam-20071221/universal $PLUGINS >> "$logfile" 2>&1
+        ./configure --host="${ARCH}-apple-darwin" --prefix=$PREFIX --enable-shared=no --enable-static=yes --with-pam=$BUILD_DIR/openpam-20071221/universal $PLUGINS >> "$logfile" 2>&1
         make -j 8 >> "$logfile" 2>&1
         if [[ "$?" != "0" ]]; then
           echo "CONFIGURE FAILED"
@@ -176,6 +190,7 @@ for TARGET in $TARGETS; do
         find . -name config.cache -print0 | xargs -0 rm
       done
 done
+
 echo "*** creating universal libs ***" >> "$logfile" 2>&1
 rm -rf "$INSTALL_PATH"
 mkdir -p "$INSTALL_PATH"
@@ -188,22 +203,27 @@ for lib in $ALL_LIBS; do
     if [[ "$dir" != "." ]]; then
         mkdir -p ${INSTALL_PATH}/lib/$dir
     fi
-    LIBS=
-    for TARGET in $TARGETS; do
-        LIBS="$LIBS ${BUILD_DIR}/${LIB_NAME}/${TARGET}${SDK_IOS_VERSION}*/lib/${lib}"
-    done
-    lipo -create ${LIBS} -output "${INSTALL_PATH}/lib/${lib}"
+
+    LIBS="${BUILD_DIR}/${LIB_NAME}/iPhoneOS${SDK_IOS_VERSION}*/lib/${lib}"
+    output="${INSTALL_PATH}/lib/iphoneos/${lib}"
+    mkdir -p "$(dirname "$output")"
+    lipo -create ${LIBS} -output "$output"
+
+    LIBS="${BUILD_DIR}/${LIB_NAME}/iPhoneSimulator${SDK_IOS_VERSION}*/lib/${lib}"
+    output="${INSTALL_PATH}/lib/iphonesimulator/${lib}"
+    mkdir -p "$(dirname "$output")"
+    lipo -create ${LIBS} -output "$output"
 done
-echo "*** creating built package ***" >> "$logfile" 2>&1
-cd "$BUILD_DIR"
-mkdir -p libsasl-ios
-cp -r "$INSTALL_PATH"/* libsasl-ios/
-tar -czf "libsasl-$version-ios.tar.gz" libsasl-ios
-mkdir -p "$resultdir"
-mv "libsasl-$version-ios.tar.gz" "$resultdir"
-cd "$resultdir"
-ln -s "libsasl-$version-ios.tar.gz" "libsasl-prebuilt-ios.tar.gz"
+
+echo "*** creating sasl2.xcframework ***"
+rm -rf $resultdir/sasl2.xcframework
+xcodebuild -create-xcframework \
+ -library $INSTALL_PATH/lib/iphoneos/libsasl2.a \
+ -headers $INSTALL_PATH/include \
+ -library $INSTALL_PATH/lib/iphonesimulator/libsasl2.a \
+ -headers $INSTALL_PATH/include \
+ -output $resultdir/sasl2.xcframework
+
 rm -rf "$tempbuilddir"
-cd "$scriptdir/.."
-tar xzf "$resultdir/libsasl-$version-ios.tar.gz"
+
 exit 0
