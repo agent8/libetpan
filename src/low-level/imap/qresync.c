bcd68dfdf7ccf5fb8871b79a4881e7cbe45f773a
Liang Wang <wangliang1124@163.com>
2024-04-17 18:23:17 +0800

    #pragma clang diagnostic ignored "-Wincompatible-pointer-types"

--- src/low-level/imap/qresync.c
+++ src/low-level/imap/qresync.c
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
+#pragma clang diagnostic ignored "-Wincompatible-pointer-types"
 #include "qresync.h"
 #include "qresync_private.h"
 
